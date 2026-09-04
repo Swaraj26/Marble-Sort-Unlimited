4077e919324d0970a85df0e3c032c7bcce9f1144
Swaraj <swarajy26@gmail.com>
2026-09-04 21:57:15 +0530

    Target SDK Changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,14 +12,14 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.swarajy.marblesortultimate"
     minSdk = 24
-    targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
+    targetSdk = 36
+    versionCode = 2
+    versionName = "1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
